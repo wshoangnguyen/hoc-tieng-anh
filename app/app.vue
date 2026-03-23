856e77249f9d662e4d00312b075f517873a7059a
ToanNM <toan.nguyenminh@dirox.net>
2026-03-23 21:24:44 +0700

    update-homepage

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<script setup>
+useHead({
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+
+const title = "Học Tiếng Anh Dễ Lắm";
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+});
+</script>
+
+<template>
+  <NuxtPage />
+</template>
